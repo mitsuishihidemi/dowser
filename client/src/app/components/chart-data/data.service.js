30b14869e3f6fada737d842b0e035804deb701fd
Science Cat <sciencecat7@gmail.com>
2016-02-21 17:24:32 -0300

    Fix graph parsing

--- client/src/app/components/chart-data/data.service.js
+++ client/src/app/components/chart-data/data.service.js
@@ -11,10 +11,23 @@
       load: load
     };
 
+    function parseData(data) {
+      return {
+        category: data.name,
+        unit: data.kind,
+        data: data.points.map(function(point) {
+          return {
+            date: point.date,
+            value: point.value
+          };
+        })
+      };
+    }
+
     function load(identifier, name) {
-      Api.__get('DataType/Get/' + identifier)
-        .then(function() {
-          var data = ChartDataMock.get();
+      Api.get('DataType/Get/' + identifier)
+        .then(function(response) {
+          var data = parseData(response.data);
           $rootScope.$emit('chart:' + identifier + ':load', data);
         });
     }
