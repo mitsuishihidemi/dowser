21afd1819f3702172baad754e666f32493279d49
mitsuishihidemi <hidemi.mitsuishi@gmail.com>
2016-02-21 10:57:29 -0300

    perfume

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -6,14 +6,15 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($state, $timeout) {
+  function MainController($state, $timeout, Particles) {
     var vm = this;
+
+    Particles.run('particles-js');
 
     vm.getStarted = function() {
       $timeout(function () {
         $state.go('wizard.register');
       }, 500);
-
     }
   }
 })();
