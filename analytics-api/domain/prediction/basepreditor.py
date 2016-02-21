91153e24564755bb1270fb6b10ed5ee6381de30d
Daniel Baptista Dias <Daniel Baptista Dias>
2016-02-21 11:39:38 -0300

    Added linear regression in API

--- analytics-api/domain/prediction/basepreditor.py
+++ analytics-api/domain/prediction/basepreditor.py
@@ -1,7 +1,16 @@
 from abc import ABCMeta
 
+
 class BasePreditor:
     __metaclass__ = ABCMeta
 
-    def predict(self, data, window):
+    def __init__(self):
+        pass
+
+    def predict(self, data, values_to_predict):
+        x = map(lambda item: float(item), data.keys())
+        y = map(lambda key: float(data[key]), data.keys())
+        return self.predict_internal(x, y, values_to_predict)
+
+    def predict_internal(self, x, y, values_to_predict):
         raise NotImplementedError()
