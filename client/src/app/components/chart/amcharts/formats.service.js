f18789b273b553cf54d9c2f270b105a6f76f2aea
Science Cat <sciencecat7@gmail.com>
2016-02-20 22:54:19 -0200

    Add new format to change lines color pallet

--- client/src/app/components/chart/amcharts/formats.service.js
+++ client/src/app/components/chart/amcharts/formats.service.js
@@ -5,6 +5,20 @@
     .module('dowser')
     .constant('AmChartFormats', {
       date: 'MMM DD',
-      dashLength: 6
+      dashLength: 6,
+      linesColorPallet: [
+        '#de4c4f',
+        '#d8854f',
+        '#eea638',
+        '#a7a737',
+        '#86a965',
+        '#8aabb0',
+        '#69c8ff',
+        '#cfd27e',
+        '#9d9888',
+        '#916b8a',
+        '#724887',
+        '#7256bc'
+      ],
     });
 })();
