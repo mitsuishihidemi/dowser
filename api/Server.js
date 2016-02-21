10154c1ff573177992b6111d25efb836580ebd89
Science Cat <sciencecat7@gmail.com>
2016-02-21 13:55:55 -0300

    Setup CORS config on server

--- api/Server.js
+++ api/Server.js
@@ -3,16 +3,26 @@ var restify = require('restify');
 var Server = function(commandFactory) {
     this.commandFactory = commandFactory;
     this.create();
+    this.setupCors();
     this.addUses();
     this.addRoutes();
 }
 
 Server.prototype.create = function() {
     this.server = restify.createServer({
-          name: 'myapp',
-          version: '1.0.0'
+        name: 'myapp',
+        version: '1.0.0'
     });
 }
+
+Server.prototype.setupCors = function() {
+    this.server.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
+};
 
 Server.prototype.addUses = function() {
     this.server.use(restify.acceptParser(this.server.acceptable));
@@ -23,8 +33,8 @@ Server.prototype.addUses = function() {
 Server.prototype.addRoutes = function() {
     var self = this;
     this.server.post('/' + this.commandFactory.ROUTE_DATA_TYPE_INSERT, function (req, res, next) {
-        var insertCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_INSERT);  
-        
+        var insertCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_INSERT);
+
         insertCommand.execute(req.params, function(error, data){
            res.send(error || data);
         });
@@ -33,8 +43,8 @@ Server.prototype.addRoutes = function() {
     });
 
     this.server.get('/' + this.commandFactory.ROUTE_DATA_TYPE_GET + "/:id", function (req, res, next) {
-        var getCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_GET);  
- 
+        var getCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_GET);
+
         getCommand.execute(req.params.id, function(error, data){
             res.send(error || data);
         });
@@ -43,8 +53,8 @@ Server.prototype.addRoutes = function() {
     });
 
     this.server.get('/' + this.commandFactory.ROUTE_DATA_TYPE_GET_BY_USER + "/:id", function (req, res, next) {
-        var getCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_GET_BY_USER);  
- 
+        var getCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_GET_BY_USER);
+
         getCommand.execute(req.params.id, function(error, data){
             res.send(error || data);
         });
@@ -53,8 +63,8 @@ Server.prototype.addRoutes = function() {
     });
 
     this.server.get('/' + this.commandFactory.ROUTE_DATA_TYPE_GET_BY_NOT_USER + "/:id", function (req, res, next) {
-        var getCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_GET_BY_NOT_USER);  
- 
+        var getCommand = self.commandFactory.create(self.commandFactory.ROUTE_DATA_TYPE_GET_BY_NOT_USER);
+
         getCommand.execute(req.params.id, function(error, data){
             res.send(error || data);
         });
