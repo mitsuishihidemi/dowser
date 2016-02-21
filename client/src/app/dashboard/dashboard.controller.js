c484539f9b3deac74b8ff82997022a14777c8dc1
mitsuishihidemi <hidemi.mitsuishi@gmail.com>
2016-02-21 09:51:41 -0300

    comparable charts

--- client/src/app/dashboard/dashboard.controller.js
+++ client/src/app/dashboard/dashboard.controller.js
@@ -6,15 +6,27 @@
     .controller('DashboardController', DashboardController);
 
   /** @ngInject */
-  function DashboardController($timeout, ChartData) {
+  function DashboardController($timeout, $state, ChartData) {
     var vm = this;
 
     vm.inputSearch = '';
     vm.chartList = [
       { dataName: 'Test' }
     ];
+    vm.comparableCharts = [
+      { dataName: 'Weather'},
+      { dataName: 'Ice Cream Selling'},
+      { dataName: 'Economic Crisis'},
+      { dataName: 'Birth of Children' }
+    ];
 
     ChartData.load('My Sells');
+
+    vm.addNewChart = function() {
+      $timeout(function () {
+        $state.go('wizard.register');
+      }, 500);
+    }
 
     vm.addChart = function(category) {
       ChartData.load(category);
