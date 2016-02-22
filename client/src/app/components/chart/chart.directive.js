fe59fd4c17ccbe920e4e75832b0b07f4c6db9259
mitsuishihidemi <hidemi.mitsuishi@gmail.com>
2016-02-21 21:54:56 -0300

    fix position

--- client/src/app/components/chart/chart.directive.js
+++ client/src/app/components/chart/chart.directive.js
@@ -24,6 +24,10 @@
     function ChartController($scope, $rootScope, $timeout, AmChartOptions, AmChartDataStore, AmChartMerge) {
       var vm = this;
 
+      if (!vm.identifier) {
+        return;
+      }
+
       vm.chartId = 'chart-' + vm.identifier
       vm.dataStore = new AmChartDataStore();
 
